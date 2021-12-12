d9a030fed744fc7210656a22ef42e0b45fb850bf
Mike Hacker <mhackersu@gmail.com>
2021-12-12 12:23:47 -0600

    adding tf and build template

---
+++ tf/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "bananaforscale-aws-tf-state"
+    key    = "react-redux-typescript-boilerplate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
---
+++ tf/vars.tf
@@ -0,0 +1,10 @@
+variable "aws_access_key" {
+  type        = string
+  default     = "aws-access-key"
+  description = "AWS IAM Access Key"
+}
+variable "aws_secret_key" {
+  type        = string
+  default     = "aws-secret-key"
+  description = "AWS IAM Secret Key"
+}
